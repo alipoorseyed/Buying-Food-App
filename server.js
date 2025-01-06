2a9d9e31a5c5414b53851d2fc2b504a38dee6550
Erfan Manouchehri <erfan2004@yahoo.com>
2025-01-06 18:41:43 +0330

    just adde a function to add manager but needs a little update latar

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-import express from "express"
-import mysql2 from "mysql2"
+import express from "express";
+import mysql2 from "mysql2";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
-console.log("hello")
-const connection = mysql2.createConnection({
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const db = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Hedie231182',
     database: 'db-project'
 });
 
-connection.connect((err) => {
+
+db.connect((err) => {
     if (err) {
         console.error('Error connecting to the database: ' + err.stack);
         return;
     }
-    console.log('Connected to the database as id ' + connection.threadId);
+    console.log('Connected to the database as id ' + db.threadId);
 });
 
-const userId = 1;
-
-connection.query('CALL GetManagerById(?)', [userId], (err, results) => {
+db.query('CALL GetManagerById(?)', [1], (err, results) => {
     if (err) {
         console.error('Error executing stored procedure: ' + err.stack);
         return;
@@ -32,9 +36,29 @@ connection.query('CALL GetManagerById(?)', [userId], (err, results) => {
 });
 
 
-connection.end();
+
+app.post("/signUp/Manager", (req, res) =>{
+    const {name, familyName, email, password, phoneNumber} = req.body;
+  
+    db.execute('CALL AddManager(?,?,?,?,?)', [name, familyName, email, password, phoneNumber], (err, result) => {
+        if (err) {
+        console.error('Error inserting user:', err);
+        res.status(500).json({ message: 'Error saving user', error: err });
+        } else {
+        res.status(201).json({
+            Managerid: result.insertId,
+            ManagerName: name,
+            ManagerFamilyName: familyName,
+            ManagerEmail: email,
+            ManagerPassword: password,
+            ManagerPhoneNumber: phoneNumber,
+        });
+        }
+    });
+
+});
 
 
 app.listen(port, () =>{
-    console.log(`LIstening on port ${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
