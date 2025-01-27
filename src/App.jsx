c831033331ce25c2d7b45b7844780897da753841
alipoorseyed <poorseyedali40@gmail.com>
2025-01-27 13:23:32 +0330

    fix[fix some issue in other pages]

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { ChooseRolePage } from './components/ChooseRolePage';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { SideBar } from './components/SideBar';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
 function App() {
-  
   return (
-    <>
-    <div dir='rtl' className='pr-32 pl-32 pt-10 pb-10'>
-    <Router>
-      <SideBar />
-      <Routes>
-        <Route path='/' element={<ChooseRolePage />} />
-      </Routes>
-    </Router>
+    <div dir="rtl" className="pr-32 pl-32 pt-10 pb-10">
+      <Router>
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<ChooseRolePage />} />
+          <Route path="/Login/:Role" element={<Login />} />
+          <Route path="/SignUp/:Role" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
