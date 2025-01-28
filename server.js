ae6326876e769340007f9ad42f13a781b06e291c
Erfan Manouchehri <erfan2004@yahoo.com>
2025-01-28 22:38:30 +0330

    added the UpdateItem post request

--- server.js
+++ server.js
@@ -369,6 +369,26 @@ app.post("/UpdateAddress", (req, res) =>{
 });
 
 
+app.post("/UpdateItem", (req, res) =>{
+    const {id, RestaurantId, ItemName, ItemPrice} = req.body;
+
+    if (!id || !RestaurantId || !ItemName || !ItemPrice) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    db.execute('CALL UpdateItem(?,?,?,?)', [id, RestaurantId, ItemName, ItemPrice], (err, result) => {
+        if (err) {
+            console.error('Error Updating Item:', err);
+            res.status(500).json({ message: 'Error Updating Item', error: err });
+        } else {
+            res.status(201).json({
+                result
+            });
+        }
+    });
+});
+
+
 app.get("/AllRestaurants", (req, res) =>{
 
     db.execute('CALL SelectAllRestaurant()', (err, results) => {
